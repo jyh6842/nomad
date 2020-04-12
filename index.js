71000078f80e76b5e4b48abd88c5f2b0c584676f
jyh6842 <jyh6842@gmail.com>
2020-04-13 00:56:57 +0900

    commit

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const title = document.querySelector("#title");
 
+const CLICKED_CLASS = "click_change_className";
+
 const BASE_COLOR = "rgb(52, 73, 94)";
 const OTHER_COLOR = "#7f8c8d";
 
 function handleClick() {
     const currentColor = title.style.color;
+    const currentClass = title.className;
+    console.log(currentClass);
+
+
+    const hasClass= title.classList.contains(CLICKED_CLASS);
+    if(hasClass){
+        title.classList.remove(CLICKED_CLASS);
+    }else{
+        title.classList.add(CLICKED_CLASS);
+    }
+
+    // if(currentClass !== CLICKED_CLASS){ 
+    //     title.className = CLICKED_CLASS;
+    // }else{
+    //     title.className = "";
+    // }
 
     if(currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR; //"#7f8c8d"
+        title.style.color = OTHER_COLOR; // "#7f8c8d"
     }else{
-        title.style.color = BASE_COLOR; //rgb(52, 73, 94)
+        title.style.color = BASE_COLOR; // rgb(52, 73, 94)
     }
+    
+
+
+
+
+
+
+
+
+
+
 }
 
 function init(){
     title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
-
 
 // 계속 실행 시킨다.
 init();
